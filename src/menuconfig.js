de34db95aa65284a823d98ac03a90a8b57ead560
Matthew Marchese <maffblaster@gentoo.org>
2015-10-23 17:01:05 -0700

    Adding examples and src file.

---
+++ src/menuconfig.js
@@ -0,0 +1,27 @@
+
+// Define global variable
+kernel_configs_dir = "./kernel_configs/";
+kernel_config_version = "x86_64-4.1.6";
+
+kernel_full = kernel_configs_dir + kernel_config_version
+
+// This is a test function
+function getbowser () {
+	alert("You are running the " + navigator.appName + " browser.")
+}
+
+// This function should read CONFIG variable input from a text box
+function get_config_version() {
+	
+}
+
+// This function should read in specified version of a kernel configuration file. Support gentoo-sources and vanilla sources
+function read_config_version() {
+  
+
+}
+
+// This function should only return/write revelant configuration variables.
+function write_config () {
+	
+}
